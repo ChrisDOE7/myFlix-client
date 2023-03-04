87ed0cabf1791eba57fba73c9f2216011cb616a7
ChrisDOE <chris.sean.86@gmail.com>
2023-03-04 10:34:08 +0100

    Update main-view.jsx
    - deleted the movie array, in preparation to the api implementation.

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -3,55 +3,17 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-   const [movies, setMovies] = useState([
-      {
-         id: 1,
-         title: "Titanic",
-         genre: "Drama",
-         year: "1997",
-         image:
-            "https://m.media-amazon.com/images/I/81XmODGX7LL._AC_UY436_QL65_.jpg",
-         director: "James Cameron"
-      },
-      {
-         id: 2,
-         title: "Goodfellas",
-         genre: "Crime",
-         year: "1990",
-         image:
-            "https://m.media-amazon.com/images/I/91llG6OCC4L._AC_UY436_QL65_.jpg",
-         director: "Martin Scorsese"
-      },
-      {
-         id: 3,
-         title: "Pulp Fiction",
-         genre: "Crime",
-         year: "1994",
-         image:
-            "https://m.media-amazon.com/images/I/71W5O2mWD7L._AC_UY436_QL65_.jpg",
-         director: "Quentin Tarantino"
-      },
-      {
-         id: 4,
-         title: "The Godfather",
-         genre: "Crime",
-         year: "1972",
-         image:
-            "https://m.media-amazon.com/images/I/715LlcDMiTL._AC_UY436_QL65_.jpg",
-         director: "Francis Ford Coppola"
-      },
-      {
-         id: 5,
-         title: "The Shawshank Redemption",
-         genre: "Drama",
-         year: "1994",
-         image:
-            "https://m.media-amazon.com/images/I/71vl5lGjv4L._AC_UY436_QL65_.jpg",
-         director: "Frank Darabont"
-      }
-   ]);
+   const [movies, setMovies] = useState([]);
 
    const [selectedMovie, setSelectedMovie] = useState(null);
+
+   useEffect(() => {
+      fetch("https://openlibrary.org/search.json?q=star+wars")
+         .then(response => response.json())
+         .then(data => {
+            console.log("books from api:", data);
+         });
+   }, []);
 
    if (selectedMovie) {
       return (
